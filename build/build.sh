930eea70206710c64388b4066f403023142bcd40
Gowtham-Sharoon <gauthamyoung005@gmail.com>
2024-04-20 18:24:29 +0530

    from master to master

--- build/build.sh
+++ build/build.sh
@@ -3,11 +3,11 @@
 #docker logins,
 sudo docker login -u gowthamsharoon -p SHArj84KMF#
 
-sudo docker build -t guvi-app-dev .
+sudo docker build -t guvi-app-prod .
 
-sudo docker tag guvi-app-dev:latest gowthamsharoon/dev:latest
+sudo docker tag guvi-app-prod:latest gowthamsharoon/prod:latest
 
-sudo docker push gowthamsharoon/dev:latest
+sudo docker push gowthamsharoon/prod:latest
 
 #build file here used to build docker image.
 
--- build/deploy.sh
+++ build/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo docker pull gowthamsharoon/dev:latest
-sudo docker stop guvi-app
-sudo docker rm guvi-app
-sudo docker run -d -p 80:80 --name guvi-app gowthamsharoon/dev:latest
+sudo docker pull gowthamsharoon/prod:latest
+sudo docker stop guvi-app-dev
+sudo docker rm guvi-app-dev
+sudo docker run -d -p 80:80 --name guvi-app gowthamsharoon/prod:latest
 
 
 #sudo docker pull gowthamsharoon/dev:latest
