c3251620fc529644f7788250efd6da3e8e638842
Gowtham-Sharoon <gauthamyoung005@gmail.com>
2024-04-20 18:47:49 +0530

    new master

--- build/build.sh
+++ build/build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-#docker logins,
+# Docker login
+# Consider using a credential helper or environment variables for secure login
 sudo docker login -u gowthamsharoon -p SHArj84KMF#
 
+# Build the Docker image
 sudo docker build -t guvi-app-prod .
 
+# Tag the built image correctly
 sudo docker tag guvi-app-prod:latest gowthamsharoon/prod:latest
 
+# Push the tagged image to Docker Hub
 sudo docker push gowthamsharoon/prod:latest
-
-#build file here used to build docker image.
-
 
